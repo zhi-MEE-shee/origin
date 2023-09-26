4b7fe0cb73b779648bd238f789a3ccb291c33b91
Oksana Sukhanova <pluviahorisontal@gmail.com>
2023-09-27 02:11:58 +0300

    Testing catch2 Task 1 & 2 added

---
+++ Тестирование в С++/Задача 2. Проверка функций добавления и получения элементов из двусвязного списка/src/main.cpp
@@ -0,0 +1,156 @@
+#include <catch2/catch_test_macros.hpp>
+#include <cstdint>
+
+#include <iostream>
+
+struct ListNode
+{
+public:
+    ListNode(int value, ListNode* prev = nullptr, ListNode* next = nullptr)
+        : value(value), prev(prev), next(next)
+    {
+        if (prev != nullptr) prev->next = this;
+        if (next != nullptr) next->prev = this;
+    }
+
+public:
+    int value;
+    ListNode* prev;
+    ListNode* next;
+};
+
+
+class List
+{
+public:
+    List()
+        : m_head(new ListNode(static_cast<int>(0))), m_size(0),
+        m_tail(new ListNode(0, m_head))
+    {
+    }
+
+    virtual ~List()
+    {
+        Clear();
+        delete m_head;
+        delete m_tail;
+    }
+
+    bool Empty() { return m_size == 0; }
+
+    unsigned long Size() { return m_size; }
+
+    void PushFront(int value)
+    {
+        new ListNode(value, m_head, m_head->next);
+        ++m_size;
+    }
+
+    void PushBack(int value)
+    {
+        new ListNode(value, m_tail->prev, m_tail);
+        ++m_size;
+    }
+
+    int PopFront()
+    {
+        if (Empty()) throw std::runtime_error("list is empty");
+        auto node = extractPrev(m_head->next->next);
+        int ret = node->value;
+        delete node;
+        return ret;
+    }
+
+    int PopBack()
+    {
+        if (Empty()) throw std::runtime_error("list is empty");
+        auto node = extractPrev(m_tail);
+        int ret = node->value;
+        delete node;
+        return ret;
+    }
+
+    void Clear()
+    {
+        auto current = m_head->next;
+        while (current != m_tail)
+        {
+            current = current->next;
+            delete extractPrev(current);
+        }
+    }
+
+private:
+    ListNode* extractPrev(ListNode* node)
+    {
+        auto target = node->prev;
+        target->prev->next = target->next;
+        target->next->prev = target->prev;
+        --m_size;
+        return target;
+    }
+
+private:
+    ListNode* m_head;
+    ListNode* m_tail;
+    unsigned long m_size;
+};
+
+
+TEST_CASE("List's_functions_test", "[pop_func]") {
+
+    List new_list;
+
+    SECTION("Adding list node to front side") {
+        for (int i = 0; i <= 10; i += 2) {
+            new_list.PushFront(i);
+        }
+
+        for (int i = 10; i >= 0; i -= 2) {
+            CHECK(new_list.PopFront() == i);
+        }
+
+    }
+
+    SECTION("Adding list node to back side") {
+       
+        new_list.PushBack(10);
+        new_list.PushBack(8);
+
+        CHECK(new_list.PopBack() == 8);
+        CHECK(new_list.PopBack() == 10);
+    }
+
+    SECTION("some other test") {
+
+        for (int i = 0; i < 10; ++i) {
+            new_list.PushFront(i);
+        }
+
+        int sum{ 0 };
+
+        while (!new_list.Empty()) {
+            sum += 1;
+            new_list.PopBack();
+        }
+        REQUIRE(sum == 10);
+    }
+
+
+    SECTION("list throws") {
+
+        //если выбрасывает исключение, то проходит тест
+        REQUIRE_THROWS_AS(new_list.PopFront(), std::runtime_error);
+
+        try {
+            new_list.PopBack();
+        }
+        catch (std::runtime_error) {
+            SUCCEED("error was catched");
+            return;
+        }
+        FAIL("no exception");
+
+    }
+
+}
