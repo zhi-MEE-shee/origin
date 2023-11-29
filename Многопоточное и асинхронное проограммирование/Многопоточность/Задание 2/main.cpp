5360a90441eadba89783dcc54dca592b9faf3948
Oksana Sukhanova <pluviahorisontal@gmail.com>
2023-11-29 22:06:42 +0300

    многопоточность задание 2 обновлено

--- Многопоточное и асинхронное проограммирование/Многопоточность/Задание 2/main.cpp
+++ Многопоточное и асинхронное проограммирование/Многопоточность/Задание 2/main.cpp
@@ -1,10 +1,11 @@
 #include <algorithm>
 #include <chrono> 
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include <vector>
 
-size_t size = 1000;
+using namespace std::chrono_literals;
 
 
 void vec_summ(const std::vector<int>&V1, const std::vector<int>&V2, std::vector<int>& res ) {
@@ -13,30 +14,45 @@ void vec_summ(const std::vector<int>&V1, const std::vector<int>&V2, std::vector<
 }
 
 
-
 int main() {
 
 	setlocale(LC_ALL, "Russian");
 
-	std::cout << "Количество аппаратных ядер: " << std::thread::hardware_concurrency() << std::endl;
+	std::cout << "Количество аппаратных ядер: " << std::thread::hardware_concurrency() << std::endl;	
 
-	std::vector<int> V1(size, 3);
-	std::vector<int> V2(size, 2);
-	std::vector<int> res(size);
+	for (int i = 1000; i < 1000001; i *= 10){ std::cout << "\t\t" << i ; }
+	std::cout << std::endl;
 
-	auto start = std::chrono::steady_clock::now();
-	
-	std::thread th (vec_summ, std::ref(V1), std::ref(V2), std::ref(res));
-	
-	auto end = std::chrono::steady_clock::now();
-	auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-	std::cout << diff.count() << " ms" << std::endl;
 
-	// для записи результата
-	//std::vector<std::chrono::milliseconds> func_time;
-	//func_time.push_back(diff);
-	
-	th.join();
+	for (int k = 1; k < 17; k *= 2) {
+
+		std::cout << k << ((k != 1) ? " threads " : " thread ");
+
+		for (int i = 1000; i < 1000001; i *= 10) {
+
+			std::vector<int> V1(i, 6);
+			std::vector<int> V2(i, 2);
+			std::vector<int> res(i);
+			
+			auto start = std::chrono::system_clock::now();
+
+			std::vector<std::thread> th;
+			for (int i = 0; i < k; ++i) {
+				th.push_back(std::thread(vec_summ, std::ref(V1), std::ref(V2), std::ref(res)));
+			}
+
+			for (auto& it : th) {
+				it.join();
+			}
+
+			auto end = std::chrono::system_clock::now();
+
+			std::chrono::duration<double> diff = end - start;
+			std::cout << "\t" << diff.count() << " ms";
+		}
+		std::cout << std::endl;
+
+	}
 
 	return 0;
 }
