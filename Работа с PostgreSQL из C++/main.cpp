943950fe42217e1d87bc3875709c5b195bc5113e
Oksana Sukhanova <pluviahorisontal@gmail.com>
2023-11-30 06:26:59 +0300

    libpqxx task added

--- Работа с PostgreSQL из C++/main.cpp
+++ Работа с PostgreSQL из C++/main.cpp
@@ -22,12 +22,12 @@ int main() {
 		);
 
 		pqxx::transaction tx{conn};
-/*
+
 		tx.transaction_base::exec(
 			"CREATE TABLE public.client ("
 			"id serial4 NOT NULL, "
-			"имя VARCHAR(40) NOT NULL, "
-			"фамилия VARCHAR(40) NOT NULL, "
+			"first_name VARCHAR(40) NOT NULL, "
+			"last_name VARCHAR(40) NOT NULL, "
 			"email VARCHAR(80) UNIQUE NOT NULL, "
 			"CONSTRAINT client_pk PRIMARY KEY(id));");
 
@@ -40,10 +40,59 @@ int main() {
 			"client_id INTEGER NOT NULL REFERENCES client(id), "
 			"phone_number_id INTEGER NOT NULL REFERENCES phone_number(id), "	
 			"PRIMARY KEY(client_id, phone_number_id));");
-*/	
+	
+	
+	
+		std::string first_name = "Джек";
+		std::string last_name = "Рассел";
+		std::string email = "JR@gmail.com";
 
-		conn.prepare("insert_client", "INSERT INTO client (имя, фамилия, email) VALUES ($1, $2, $3);");
-		conn.prepare("insert_phone", "INSERT INTO client_phone (имя, фамилия, email) VALUES ($1, $2, $3);");
+		std::string client_id = "INSERT INTO public.client (id, first_name, last_name, email) VALUES (DEFAULT,'" + tx.esc(first_name) + "', '" + tx.esc(last_name) + "', '" + tx.esc(email) + "') RETURNING id;";
+		pqxx::result c = tx.exec(client_id);
+			if (c.size() == 1) {
+				int id = c[0][0].as<int>();
+				std::cout << "Добавлен клиент с номером ID: " << id << std::endl
+						  << "Имя: " << first_name << " " << last_name << std::endl
+						  << "email: " << email << std::endl;
+			}
+		
+		std::string phone = "123456789"; 
+		std::string p_id = "INSERT INTO public.phone_number (id, phone) VALUES(DEFAULT, '" + phone + "') RETURNING id;";
+		pqxx::result r = tx.exec(p_id);
+
+			if (r.size() == 1) { 
+				int id = r[0][0].as<int>(); 
+				std::cout << "Добавлен ID: " << id << std::endl
+						  << "Для номера: " << phone << std::endl;
+			}
+
+		std::string cli_phone = "INSERT INTO client_phone (client_id, phone_number_id) VALUES(1, 1)";
+		tx.exec(cli_phone);
+
+
+		std::string new_first_name = "Том";
+		std::string new_name = "UPDATE public.client SET first_name = '" + new_first_name +"' WHERE id = 1";
+		tx.exec(new_name);
+		std::cout << "Имя было изменено на: " << new_first_name << std::endl;
+
+
+		auto collection = tx.query <std::string, std::string>("SELECT c.first_name, ph.phone FROM public.client c "
+			"JOIN public.client_phone cp ON c.id = cp.client_id "
+			"JOIN public.phone_number ph ON cp.phone_number_id = ph.id "
+			"WHERE ph.id = 3 ");
+		for (auto& record : collection) {
+			std::string name = std::get<0>(record);
+			std::string phone = std::get<1>(record);
+
+			std::cout << name << "'s phone number is " << phone << std::endl;
+		}
+
+
+
+		pqxx::result d1 = tx.exec("DELETE FROM public.client_phone WHERE client_id = 1");
+		pqxx::result d2 = tx.exec("DELETE FROM public.phone_number WHERE id = 1");
+		pqxx::result d3 = tx.exec("DELETE FROM public.client WHERE first_name = 'Том'");
+
 
 		tx.commit();
 
@@ -52,9 +101,6 @@ int main() {
 	{
 		std::cout << e.what() << std::endl;
 	}
-
-
-	std::cout << "hello!";
 
 	return 0;
 }
